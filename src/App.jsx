db31d4baf4cb88f7e2629a910892b239390a654a
faheeeem1 <172581972+faheeeem1@users.noreply.github.com>
2026-09-10 18:34:20 +0530

    Fix routing and GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Activities from "./pages/Activities";
@@ -12,70 +11,68 @@ import MainLayout from "./components/MainLayout";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
 
-        <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/" element={<Navigate to="/login" />} />
 
-        <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <MainLayout>
-                <Dashboard />
-              </MainLayout>
-            </ProtectedRoute>
-          }
-        />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <MainLayout>
+              <Dashboard />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
 
-        <Route
-          path="/activities"
-          element={
-            <ProtectedRoute>
-              <MainLayout>
-                <Activities />
-              </MainLayout>
-            </ProtectedRoute>
-          }
-        />
+      <Route
+        path="/activities"
+        element={
+          <ProtectedRoute>
+            <MainLayout>
+              <Activities />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
 
-        <Route
-          path="/add-activity"
-          element={
-            <ProtectedRoute>
-              <MainLayout>
-                <AddActivity />
-              </MainLayout>
-            </ProtectedRoute>
-          }
-        />
+      <Route
+        path="/add-activity"
+        element={
+          <ProtectedRoute>
+            <MainLayout>
+              <AddActivity />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
 
-        <Route
-          path="/categories"
-          element={
-            <ProtectedRoute>
-              <MainLayout>
-                <Categories />
-              </MainLayout>
-            </ProtectedRoute>
-          }
-        />
+      <Route
+        path="/categories"
+        element={
+          <ProtectedRoute>
+            <MainLayout>
+              <Categories />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
 
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <MainLayout>
-                <Profile />
-              </MainLayout>
-            </ProtectedRoute>
-          }
-        />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          </ProtectedRoute>
+        }
+      />
 
-      </Routes>
-    </BrowserRouter>
+    </Routes>
   );
 }
 
